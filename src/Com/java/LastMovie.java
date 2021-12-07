23e838ca937ba15dc6f0348215984b6fbb63e9ba
Divyasalagundla <Divyasalagundla1@gmail.com>
2021-12-07 10:38:32 -0500

    Initial commit

---
+++ src/Com/java/LastMovie.java
@@ -0,0 +1,27 @@
+package Com.java;
+
+public class LastMovie {
+
+	private static int nnumber;
+
+	public static void main(String[] args) {
+	
+	
+	
+	
+	if(nnumber >=1 & nnumber <= 8) {
+		
+		if(nnumber== 1)
+		System.out.println("first movie name :Harry Potter and the Philosopher's Stone");
+	}
+	else if(nnumber==8) {
+		System.out.println("last movie name: Harry Potter and the Deathly Hallows – Part 2");                      //for first and last movie
+	}
+	else
+	{
+		System.out.println("not first or last movie");
+	}
+
+}
+	}
+
